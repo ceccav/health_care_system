f0ca694296e079d9274de86261be8a3519ced00f
ceccav <ceciliacavallin@hotmail.se>
2025-10-15 14:02:07 +0200

    Made changes in program.cs
    cleaned up the menu, added tryclear

--- Program.cs
+++ Program.cs
@@ -1,81 +1,97 @@
-﻿using App;
+﻿using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+using System.Security;
+using App;
 
 List<User> users = new List<User>();
-bool loginmenu = true;
+bool running = true;
+User? active_user = null;
 //Reads in all users from data/users.txt
 users = Save_System.ReadLogins();
 
-while (loginmenu)
+
+
+while (running)
 {
-    Console.WriteLine("Select a option");
-    Console.WriteLine("1. Register as a patient");
-    Console.WriteLine("2. Log in");
-    Console.WriteLine("3. Quit");
-    string? option = Console.ReadLine();
 
-    switch (option)
+    if (active_user == null)
     {
-        case "1":
-            Console.Write("Enter your username: ");
-            string? newusername = Console.ReadLine();
+        Console.WriteLine("Select a option");
+        Console.WriteLine("1. Register as a patient");
+        Console.WriteLine("2. Log in");
+        Console.WriteLine("3. Quit");
+        string? option = Console.ReadLine();
 
-            Console.Write("Enter your password : ");
-            string? _newpassword = Console.ReadLine();
+        switch (option)
+        {
 
-            if (string.IsNullOrWhiteSpace(newusername) || string.IsNullOrWhiteSpace(_newpassword))
-            {
-                Console.WriteLine("Username and password cannot be empty!");
-                return;
-            }
+            case "1":
+                Console.Write("Enter your username: ");
+                string? newusername = Console.ReadLine();
 
-            //Controls if the username already exists
-            bool userExists = users.Exists(u => u.Username.Equals(newusername, StringComparison.OrdinalIgnoreCase));
-            if (userExists)
-            {
-                Console.WriteLine("That username is already taken.");
-                return;
-            }
+                Console.Write("Enter your password : ");
+                string? _newpassword = Console.ReadLine();
 
-            //create new user and add to the list
-            User newuser = new User(newusername, _newpassword);
-            users.Add(newuser);
-
-            //save user to the file
-            Save_System.SaveLogin(newusername, _newpassword);
-            Console.WriteLine("Your account have been created");
-
-            break;
-        case "2":
-            Console.Write("Enter your username: ");
-            string? loginUser = Console.ReadLine();
-
-            Console.Write(" Enter your password: ");
-            string? loginPassword = Console.ReadLine();
-
-
-
-            bool loggedIn = false;
-            User currentUser = null;
-
-            //check if the user is in the list
-            foreach (User user in users)
-            {
-                if (user.TryLogin(loginUser, loginPassword))
+                if (string.IsNullOrWhiteSpace(newusername) || string.IsNullOrWhiteSpace(_newpassword))
                 {
-                    loggedIn = true;
-                    currentUser = user;
-                    Console.WriteLine(" Welcome " + loginUser);
-                    break;
+                    Console.WriteLine("Username and password cannot be empty!");
+                    return;
                 }
 
-            }
-            break;
-        case "3":
-            loginmenu = false;
-            break;
+                //Controls if the username already exists
+                bool userExists = users.Exists(u => u.Username.Equals(newusername, StringComparison.OrdinalIgnoreCase));
+                if (userExists)
+                {
+                    Console.WriteLine("That username is already taken.");
+                    return;
+                }
+
+                //create new user and add to the list
+                User newuser = new User(newusername, _newpassword);
+                users.Add(newuser);
+
+                //save user to the file
+                Save_System.SaveLogin(newusername, _newpassword);
+                Console.WriteLine("Your account have been created");
+
+                break;
+            case "2":
+                Console.Write("Enter your username: ");
+                string? loginUser = Console.ReadLine();
+
+                Console.Write(" Enter your password: ");
+                string? loginPassword = Console.ReadLine();
+
+
+                //check if the user is in the list
+                foreach (User user in users)
+                {
+                    if (user.TryLogin(loginUser, loginPassword))
+                    {
+                        active_user = user;
+                        Console.WriteLine(" Welcome " + loginUser);
+                        return;
+                    }
+
+                }
+                break;
+            case "3":
+                running = false;
+                break;
+        }
     }
+    else
+    {
+        TryClear();
+
+    }
+
 }
 
+void TryClear()
+{
+    try { Console.Clear(); } catch { }
+}
 
 
 
