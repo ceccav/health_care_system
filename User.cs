6294b343c3644cc37710075eb9bb73359423eb72
joelsjovall <joel.sjovall@gmail.com>
2025-10-15 11:24:23 +0200

    cleaned up program.cs

--- User.cs
+++ User.cs
@@ -66,3 +66,15 @@ public class User
 //     Patient
 // }
 
+
+// namespace App;
+
+// public interface IRole
+// {
+//     Role GetRoleType();     //returns which role it is
+//     bool HasPermission(string action);      //checks if the role has permission
+//     string GetRegion();    //vilken region den tillhör 
+// }
+
+
+
